fb7882f19735a6f7acb4728db504ab95e5c9a7d6
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-24 03:53:12 +0000

    Implement wordsearch.js and supplement test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,16 @@
+/**
+ *  Pair programming effort by:
+ *    - Joshua Tan
+ *    - Alfonso Lotina
+ *    - Saad Islam
+ */
+
 const chai = require('chai');
 const assert = chai.assert;
-
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +21,11 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
-
+    ], 'FRANK');
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +36,27 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+    assert.isTrue(result);
+  });
+  
+  it("should return false if it is an empty array", function() {
+    const result = wordSearch([], 'SEINFELD');
+    assert.isFalse(result);
+  });
 
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'D', 'E', 'L', 'D'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
     assert.isTrue(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/**
+ *  Pair programming effort by:
+ *    - Joshua Tan
+ *    - Alfonso Lotina
+ *    - Saad Islam
+ */
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  let verticalLetters = [];
+  const colSize = letters.length;
+
+  for (let i = 0; i < colSize; i++) {
+    let wordArr = [];
+
+    for (const array of letters) {
+      wordArr.push(array[i]);
+    }
+
+    verticalLetters.push(wordArr);
+  }
+
+  const verticalJoin = verticalLetters.map(ls => ls.join(''));
+
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
